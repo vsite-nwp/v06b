baf36e501e4b554b96dedbb2f881e57bf658844e
vanjs83 <tihomir.vanjurek@gmail.com>
2015-11-29 16:52:54 +0100

    create function OnOk

--- main.cpp
+++ main.cpp
@@ -1,15 +1,38 @@
 #include "main.h"
 #include "rc.h"
+bool GetFont(LOGFONT& lf, COLORREF& col) {
+	CHOOSEFONT cf;
+	ZeroMemory(&cf, sizeof cf);
+	cf.lStructSize = sizeof cf;
+	cf.Flags = CF_INITTOLOGFONTSTRUCT | CF_SCREENFONTS | CF_EFFECTS;
+	cf.lpLogFont = &lf;
+	cf.rgbColors = col;
+	if (ChooseFont(&cf)) {
+		col = cf.rgbColors;
+		return true;
+	}
+	return false;
+}
+
 
 int NumberDialog::IDD(){
 	return IDD_NUMBER; 
 }
 bool NumberDialog::OnInitDialog(){
+	SetInt(IDC_EDIT1, n);
 	return true;
 }
 bool NumberDialog::OnOK(){
+	try {
+		n = GetInt(IDC_EDIT1);
+	}
+	catch (XCtrl&) {
+		MessageBox(*this, _T("Primam samo brojke!"), NULL, MB_OK);
+		return false;
+	}
 	return true;
 }
+
 
  
 void MainWindow::OnPaint(HDC hdc){
@@ -18,6 +41,9 @@ void MainWindow::OnPaint(HDC hdc){
 void MainWindow::OnCommand(int id){
 	switch(id){
 		case ID_FONT: 
+			if (GetFont(lf, col))
+				InvalidateRect(*this, NULL, true);
+
 			break;
 		case ID_NUMBER: 
 			break;
