e626a1d17c6226da37ddbf7e7eb7e99537536127
Student <Student@VSITE>
2016-11-28 19:21:55 +0100

    Sve odjednom

--- main.cpp
+++ main.cpp
@@ -5,21 +5,57 @@ int NumberDialog::IDD(){
 	return IDD_NUMBER; 
 }
 bool NumberDialog::OnInitDialog(){
+	SetInt(IDC_EDIT1, req_number);
 	return true;
 }
 bool NumberDialog::OnOK(){
+	try { req_number = GetInt(IDC_EDIT1); }
+	catch (XCtrl msg) {
+		MessageBox(*this, _T("Uneseni znak nije broj."), NULL, MB_OK); return false; }
 	return true;
 }
 
  
 void MainWindow::OnPaint(HDC hdc){
-}
+	RECT client_rect;
+	GetClientRect(*this, &client_rect);
+	
+	MoveToEx(hdc, 0, client_rect.bottom / max_number, NULL);
+	LineTo(hdc, client_rect.right, client_rect.bottom / max_number);
 
+	MoveToEx(hdc, client_rect.right / max_number, 0, NULL);
+	LineTo(hdc, client_rect.right / max_number, client_rect.bottom);
+
+	TCHAR num[15];
+	for (int count = 1; count <= max_number; ++count) { // okomiti brojevi
+		_stprintf(num, _T("%d"), count);
+		RECT rect = { 0, count * client_rect.bottom / max_number, client_rect.right / max_number, (count + 1) * client_rect.bottom / max_number };
+		DrawText(hdc, num, -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+	}
+
+	for (int count = 1; count <= max_number; ++count) { // vodoravni brojevi
+		_stprintf(num, _T("%d"), count);
+		RECT rect = { count * client_rect.right / max_number, 0, (count + 1) * client_rect.right / max_number, client_rect.bottom / max_number };
+		DrawText(hdc, num, -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+		for (int mp = 1; mp <= max_number; ++mp) { //mnozenje
+			_stprintf(num, _T("%d"), count*mp);
+			RECT rect = { count * client_rect.right / max_number, mp * client_rect.bottom / max_number, (count + 1) * client_rect.right / max_number, (mp + 1) * client_rect.bottom / max_number };
+			DrawText(hdc, num, -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+		}
+	}
+}
+	
 void MainWindow::OnCommand(int id){
 	switch(id){
 		case ID_FONT: 
+			if (make_font(lf, color)) InvalidateRect(*this, NULL, true);
 			break;
-		case ID_NUMBER: 
+		case ID_NUMBER:
+		{
+			NumberDialog dlg;
+			dlg.req_number = max_number;
+			if (dlg.DoModal(0, *this) == IDOK) { max_number = dlg.req_number; InvalidateRect(*this, NULL, true); }
+		}
 			break;
 		case ID_EXIT: 
 			DestroyWindow(*this); 
--- main.h
+++ main.h
@@ -2,6 +2,8 @@
 #include "nwpdlg.h"
 
 class NumberDialog : public Dialog {
+public:
+	int req_number; //requested number
 protected:
 	int IDD();
 	bool OnInitDialog();
@@ -9,8 +11,31 @@ protected:
 };
  
 class MainWindow : public Window {
+private:
+	LOGFONT lf;
+	COLORREF color;
+	int max_number;
+public:
+	MainWindow() : max_number(15), color(RGB(0, 0, 0)) {
+		ZeroMemory(&lf, sizeof(lf));
+		_tcscpy(lf.lfFaceName,_T("Arial"));
+		HDC hdc = GetDC(0);
+		lf.lfHeight = -16 * GetDeviceCaps(hdc, LOGPIXELSY) / 72;
+		ReleaseDC(0, hdc);
+	}
 protected:
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
 	void OnDestroy();
 };
+
+bool make_font(LOGFONT &lf, COLORREF &color) {
+	CHOOSEFONT cf;
+	ZeroMemory(&cf, sizeof(cf));
+	cf.lStructSize = sizeof(cf);
+	cf.rgbColors = color;
+	cf.lpLogFont = &lf;
+	cf.Flags = CF_INITTOLOGFONTSTRUCT | CF_SCREENFONTS;
+	if (ChooseFont(&cf)) { color = cf.rgbColors; return true; } 
+	return false;
+}
