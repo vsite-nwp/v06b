e0c3a6aac392c00e62bd89321a634d5675efd7a9
Student <Student@VSITE>
2016-11-29 12:57:52 +0100

    Dodao promjenu fonta u OnPaint()

--- main.cpp
+++ main.cpp
@@ -19,30 +19,39 @@ bool NumberDialog::OnOK(){
 void MainWindow::OnPaint(HDC hdc){
 	RECT client_rect;
 	GetClientRect(*this, &client_rect);
-	
-	MoveToEx(hdc, 0, client_rect.bottom / max_number, NULL);
-	LineTo(hdc, client_rect.right, client_rect.bottom / max_number);
 
-	MoveToEx(hdc, client_rect.right / max_number, 0, NULL);
-	LineTo(hdc, client_rect.right / max_number, client_rect.bottom);
+	HFONT new_font = CreateFontIndirect(&lf);
+	HGDIOBJ old_font = SelectObject(hdc, new_font);
+
+	float dx = client_rect.right / (max_number + 1);
+	float dy = client_rect.bottom / (max_number +1);
+
+	MoveToEx(hdc, 0, dy, NULL);
+	LineTo(hdc, client_rect.right, dy);
+
+	MoveToEx(hdc, dx, 0, NULL);
+	LineTo(hdc, dx, client_rect.bottom);
 
 	TCHAR num[15];
 	for (int count = 1; count <= max_number; ++count) { // okomiti brojevi
 		_stprintf(num, _T("%d"), count);
-		RECT rect = { 0, count * client_rect.bottom / max_number, client_rect.right / max_number, (count + 1) * client_rect.bottom / max_number };
+		RECT rect = { 0, count * dy, dx, (count + 1) * dy };
 		DrawText(hdc, num, -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
 	}
 
 	for (int count = 1; count <= max_number; ++count) { // vodoravni brojevi
 		_stprintf(num, _T("%d"), count);
-		RECT rect = { count * client_rect.right / max_number, 0, (count + 1) * client_rect.right / max_number, client_rect.bottom / max_number };
+		RECT rect = { count * dx, 0, (count + 1) * dx, dy };
 		DrawText(hdc, num, -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
 		for (int mp = 1; mp <= max_number; ++mp) { //mnozenje
 			_stprintf(num, _T("%d"), count*mp);
-			RECT rect = { count * client_rect.right / max_number, mp * client_rect.bottom / max_number, (count + 1) * client_rect.right / max_number, (mp + 1) * client_rect.bottom / max_number };
+			RECT rect = { count * dx, mp * dy, (count + 1) * dx, (mp + 1) * dy };
 			DrawText(hdc, num, -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
 		}
 	}
+	SelectObject(hdc, old_font);
+	DeleteObject(new_font);
+
 }
 	
 void MainWindow::OnCommand(int id){
