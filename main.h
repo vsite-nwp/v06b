d32667f184103d0ed9e615c7194ae8e45242f411
Dijana Molnar <dijanamolnar89@gmail.com>
2017-11-20 19:22:14 +0100

    Ispravljena visina fonta

--- main.h
+++ main.h
@@ -18,6 +18,9 @@ public:
 	{
 		::ZeroMemory(&lf, sizeof(lf));
 		_tcscpy(lf.lfFaceName, _T("Arial"));
+		HDC hdc = GetDC(0);
+		lf.lfHeight = -18*GetDeviceCaps(hdc, LOGPIXELSY) / 72;
+		ReleaseDC(0, hdc);
 	}
 protected:
 	void OnPaint(HDC hdc);
