7206670d1d13d2127c7db0d8041afdae2ad3c0ad
banov.josipa <banov.josipa@gmail.com>
2020-01-19 14:41:57 +0100

    Multiplication table done?

--- main.cpp
+++ main.cpp
@@ -1,26 +1,79 @@
 #include "main.h"
 #include "rc.h"
 
+bool GetFont(LOGFONT& lf, COLORREF& col) {
+	CHOOSEFONT cf;
+	ZeroMemory(&cf, sizeof cf);
+	cf.lStructSize = sizeof cf;
+	cf.Flags = CF_INITTOLOGFONTSTRUCT | CF_SCREENFONTS | CF_EFFECTS;
+	cf.lpLogFont = &lf;
+	cf.rgbColors = col;
+	if (ChooseFont(&cf)) {
+		col = cf.rgbColors;
+		return true;
+	}
+	return false;
+}
+
 int NumberDialog::IDD(){
 	return IDD_NUMBER; 
 }
 bool NumberDialog::OnInitDialog(){
+	SetInt(IDC_EDIT1, number);
 	return true;
 }
 bool NumberDialog::OnOK(){
+	try {
+		number = GetInt(IDC_EDIT1);
+	}
+	catch (XCtrl&){
+		MessageBox(*this, _T("Works for the decadal numbering system only!"), NULL, MB_OK);
+		return false;
+	}
 	return true;
 }
 
  
 void MainWindow::OnPaint(HDC hdc){
+	RECT rc; GetClientRect(*this, &rc);
+	MoveToEx(hdc, 0, rc.bottom / maxNumber, NULL);	
+	LineTo(hdc, rc.right, rc.bottom / maxNumber);
+
+	MoveToEx(hdc, rc.right / maxNumber, 0, NULL);   
+	LineTo(hdc, rc.right / maxNumber, rc.bottom);
+
+	TCHAR s[10];
+	for (int i = 0; i < maxNumber + 1; ++i) {
+		_stprintf(s, _T("%d"), i);
+		RECT r = { 0, i * rc.bottom / maxNumber, rc.right / maxNumber, (i + 1) * rc.bottom / maxNumber };
+		DrawText(hdc, s, -1, &r, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+	}
+	for (int i = 1; i < maxNumber + 1; ++i) {
+		_stprintf(s, _T("%d"), i);
+		RECT r = { i * rc.right / maxNumber, 0 ,(i + 1) * rc.right / maxNumber,rc.bottom / maxNumber };
+		DrawText(hdc, s, -1, &r, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+		for (int j = 1; j < maxNumber + 1; ++j) {
+			_stprintf(s, _T("%d"), i * j);
+			RECT r = { i * rc.right / maxNumber,j * rc.bottom / maxNumber,(i + 1) * rc.right / maxNumber,(j + 1) * rc.bottom / maxNumber };
+			DrawText(hdc, s, -1, &r, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+		}
+	}
 }
 
 void MainWindow::OnCommand(int id){
 	switch(id){
 		case ID_FONT: 
+			if (GetFont(logFont, colorRef))
+				InvalidateRect(*this, NULL, true);
 			break;
-		case ID_NUMBER: 
+		case ID_NUMBER: {
+			NumberDialog dlg;	dlg.number = maxNumber;
+			if (dlg.DoModal(0, *this) == IDOK) {
+				maxNumber = dlg.number;
+				InvalidateRect(*this, NULL, true);
+			}
 			break;
+		}
 		case ID_EXIT: 
 			DestroyWindow(*this); 
 			break;
--- main.h
+++ main.h
@@ -2,6 +2,9 @@
 #include "nwpdlg.h"
 
 class NumberDialog : public Dialog {
+public:
+	int number;
+
 protected:
 	int IDD();
 	bool OnInitDialog();
@@ -9,6 +12,17 @@ protected:
 };
  
 class MainWindow : public Window {
+	LOGFONT logFont;
+	COLORREF colorRef;
+	int maxNumber;
+public:
+	MainWindow() : maxNumber(10), colorRef(RGB(0, 0, 0)) {
+		ZeroMemory(&logFont, sizeof logFont);
+		_tcscpy(logFont.lfFaceName, _T("Aria"));
+		HDC hdc = GetDC(0);
+		logFont.lfHeight = -12 * GetDeviceCaps(hdc, LOGPIXELSY) / 72;
+		ReleaseDC(0, hdc);
+	}
 protected:
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
