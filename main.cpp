54dbd5b27da3e3c81f2c0cb272a11d9241b46119
Nenad Čaklović <nenad.caklovic@reversinglabs.com>
2021-03-29 11:55:07 +0200

    Update project, add namespace, rename methods

--- main.cpp
+++ main.cpp
@@ -1,42 +1,39 @@
 #include "main.h"
 #include "rc.h"
 
-int NumberDialog::IDD(){
+int number_dialog::idd() const {
 	return IDD_NUMBER; 
 }
-bool NumberDialog::OnInitDialog(){
+bool number_dialog::on_init_dialog(){
 	return true;
 }
-bool NumberDialog::OnOK(){
+bool number_dialog::on_ok(){
 	return true;
 }
-
  
-void MainWindow::OnPaint(HDC hdc){
+void main_window::on_paint(HDC hdc){
 }
 
-void MainWindow::OnCommand(int id){
+void main_window::on_command(int id){
 	switch(id){
 		case ID_FONT: 
 			break;
 		case ID_NUMBER: 
 			break;
 		case ID_EXIT: 
-			DestroyWindow(*this); 
+			::DestroyWindow(*this); 
 			break;
 	}
 }
 
-void MainWindow::OnDestroy(){
+void main_window::on_destroy(){
 	::PostQuitMessage(0);
 }
 
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
-	Application app;
-	MainWindow wnd;	
-	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, _T("NWP"),
-		(int)LoadMenu(hInstance, MAKEINTRESOURCE(IDM_MAIN)));	
-	return app.Run();
+	vsite::nwp::application app;
+	main_window wnd;	
+	wnd.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, _T("NWP"), (int)LoadMenu(hInstance, MAKEINTRESOURCE(IDM_MAIN)));	
+	return app.run();
 }
