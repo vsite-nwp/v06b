b8153d267f516dab25e5f32d4528031b8d0f8311
Ivan Bitunjac <ivbitunj@vsite.hr>
2021-04-21 14:52:28 +0200

    Fixed v06b

--- main.cpp
+++ main.cpp
@@ -1,44 +1,19 @@
 #include "main.h"
 #include "rc.h"
 
-class multiplication {
-	inline static int numToMultiplyTo = 0;
-	inline static LOGFONT font;
-	inline static COLORREF font_color = RGB(0, 0, 0);
-	inline static CHOOSEFONT font_specs;
-public:
-	static void set_num(int number) { numToMultiplyTo = number; }
-	static int get_num() { return numToMultiplyTo; }
 
-	//Prepares font_specs for font dialog box
-	static bool choose_font() {
-		ZeroMemory(&font, sizeof(font));
-		ZeroMemory(&font_specs, sizeof(font_specs));
-		font_specs.lStructSize = sizeof(font_specs);
-		font_specs.Flags = CF_INITTOLOGFONTSTRUCT | CF_SCREENFONTS | CF_EFFECTS;
-		font_specs.lpLogFont = &font;
-		font_specs.rgbColors = font_color;
+bool choose_font(LOGFONT& log_font_ref, COLORREF& color_ref) {
+	//Prepare font specs for LOGFONT 
+	CHOOSEFONT font_specs;
+	ZeroMemory(&log_font_ref, sizeof(log_font_ref));
+	ZeroMemory(&font_specs, sizeof(font_specs));
+	font_specs.lStructSize = sizeof(font_specs);
+	font_specs.Flags = CF_INITTOLOGFONTSTRUCT | CF_SCREENFONTS | CF_EFFECTS;
+	font_specs.lpLogFont = &log_font_ref;
+	font_specs.rgbColors = color_ref;
 
-		if (ChooseFont(&font_specs)) return true;
-		return false;
-	}
-
-	static LOGFONT get_font() { return font; }
-	static COLORREF get_fontColor() { return font_color; }
-};
-
-void draw_lines(HDC context, HWND wnd_handle) {
-	RECT window_size;
-	GetClientRect(wnd_handle, &window_size);
-	int num_to_multiply_to = multiplication::get_num() + 1;
-
-	//Draws vertical line of table
-	MoveToEx(context, 0, window_size.bottom / num_to_multiply_to, NULL);
-	LineTo(context, window_size.right, window_size.bottom / num_to_multiply_to);
-
-	//Draws horizontal line of table
-	MoveToEx(context, window_size.right / num_to_multiply_to, 0, NULL);
-	LineTo(context, window_size.right / num_to_multiply_to, window_size.bottom);
+	if (ChooseFont(&font_specs)) return true;
+	return false;
 }
 
 int number_dialog::idd() const {
@@ -46,12 +21,13 @@ int number_dialog::idd() const {
 }
 
 bool number_dialog::on_init_dialog() {
+	dialog::set_int(IDC_EDIT1, this->num_to_multiply_to);
 	return true;
 }
 
 bool number_dialog::on_ok(){
 	try {
-		multiplication::set_num(dialog::get_int(IDC_EDIT1));
+		num_to_multiply_to = dialog::get_int(IDC_EDIT1);
 	}
 	catch (std::runtime_error) {
 		MessageBox(*this, _T("Error while entering number!"), 0, MB_OK);
@@ -61,45 +37,53 @@ bool number_dialog::on_ok(){
 }
  
 void main_window::on_paint(HDC hdc){
-	draw_lines(hdc, *this);
-	HFONT font_handle = CreateFontIndirect(&multiplication::get_font());
+	//If no font is selected use Times New Roman
+	if (font.lfFaceName == NULL) wcsncpy(font.lfFaceName, _T("Times New Roman"), sizeof("Times New Roman"));
+	font.lfHeight = -12 * (GetDeviceCaps(hdc, LOGPIXELSY) / 72);
+	int limit = this->limit_num + 1;
+
+	RECT window_size;
+	GetClientRect(*this, &window_size);
+
+	//Draws vertical line of table
+	MoveToEx(hdc, 0, window_size.bottom / limit, NULL);
+	LineTo(hdc, window_size.right, window_size.bottom / limit);
+
+	//Draws horizontal line of table
+	MoveToEx(hdc, window_size.right / limit, 0, NULL);
+	LineTo(hdc, window_size.right / limit, window_size.bottom);
+
+
+	HFONT font_handle = CreateFontIndirect(&font);
 	UINT font_styles = DT_CENTER | DT_VCENTER | DT_SINGLELINE;
 	HGDIOBJ font_gdiObject = SelectObject(hdc, font_handle);
-	SetTextColor(hdc, multiplication::get_fontColor());
+	SetTextColor(hdc, font_color);
 
-	RECT window_size; 
-	GetClientRect(*this, &window_size);
 	
 	TCHAR num_char[8];
-	int num_to_multiply_to = multiplication::get_num() + 1;
 
 	//Draws numbers to multiply to left and top side of multiplication table
-	for (int j = 1; j < num_to_multiply_to; ++j) {
-
-		//Write num as unicode char string to num_char
-		swprintf_s(num_char, _T("%d"), j); 
-
-		//Move rectangle in which number is to be written in
-		RECT vertical_num_rect = { 0, j * window_size.bottom / num_to_multiply_to, window_size.right / num_to_multiply_to, (j + 1) * window_size.bottom / num_to_multiply_to };
-		DrawText(hdc, num_char, -1, &vertical_num_rect, font_styles);
+	for (int j = 1; j < limit; ++j) {
 
 		swprintf_s(num_char, _T("%d"), j);
-		RECT horizontal_num_rect = { j * (window_size.right / num_to_multiply_to), 0, (j + 1) * (window_size.right / num_to_multiply_to), (window_size.bottom / num_to_multiply_to) };
+
+		//Move rectangle in which number is to be written in
+		RECT vertical_num_rect = { 0, j * window_size.bottom / limit, window_size.right / limit, (j + 1) * window_size.bottom / limit };
+		DrawText(hdc, num_char, -1, &vertical_num_rect, font_styles);
+
+		RECT horizontal_num_rect = { j * (window_size.right / limit), 0, (j + 1) * (window_size.right / limit), (window_size.bottom / limit) };
 		DrawText(hdc, num_char, -1, &horizontal_num_rect, font_styles);
 	}
 
 	//Draws and calculates multiplied numbers
-	for (int j = 1; j < num_to_multiply_to; ++j) {
-		for (int k = 1; k < num_to_multiply_to; ++k) {
-			//Calculated current multiplied number
-			int multiplied_num = j * k;
+	for (int j = 1; j < limit; ++j) {
+		for (int k = 1; k < limit; ++k) {
 
-			//Write multiplied num as char string to num_char
-			swprintf_s(num_char, _T("%d"), multiplied_num);
-			
+			swprintf_s(num_char, _T("%d"), j * k);
+
 			//Move rectangle in which number is to be written in
-			RECT multiplied_nums_rect = { j * (window_size.right / num_to_multiply_to), k * (window_size.bottom / num_to_multiply_to),
-			(j + 1) * (window_size.right / num_to_multiply_to), (k + 1) * (window_size.bottom / num_to_multiply_to) };
+			RECT multiplied_nums_rect = { j * (window_size.right / limit), k * (window_size.bottom / limit),
+			(j + 1) * (window_size.right / limit), (k + 1) * (window_size.bottom / limit) };
 			DrawText(hdc, num_char, -1, &multiplied_nums_rect, font_styles);
 		}
 	}
@@ -111,14 +95,15 @@ void main_window::on_command(int id){
 	number_dialog numDlg;
 	switch(id){
 		case ID_FONT: 
-			if (multiplication::choose_font()) {
+			if (choose_font(font, font_color)) {
 				InvalidateRect(*this, 0, true);
-				break;
 			}
 			break;
 		case ID_NUMBER: 
-			numDlg.do_modal(0, *this);
-			InvalidateRect(*this, 0, true);
+			if (numDlg.do_modal(0, *this) == IDOK) {
+				limit_num = numDlg.num_to_multiply_to;
+				InvalidateRect(*this, 0, true);
+			}
 			break;
 		case ID_EXIT: 
 			::DestroyWindow(*this); 
